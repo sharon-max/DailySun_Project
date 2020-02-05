988ff07479f32a5a887243a468672944f44b8577
livhuwani <livhuwani@adadev.org.za>
2020-02-05 15:52:57 +0200

    added changes

--- js/weatherPage.js
+++ js/weatherPage.js
@@ -6,3 +6,10 @@
 
 
 
+ function openNav() {
+    document.getElementById("mySidenav").style.width = "360px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }

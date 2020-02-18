4a4e46f5ce6ba6dac8e0be46dacee043982219b7
beauty <nunuzango@gmail.com>
2020-02-18 09:26:27 +0200

    final opinions page

--- js/opinionPage.js
+++ js/opinionPage.js
@@ -1,48 +1,34 @@
-// const popUp = document.getElementById("myPopUp");
-
-// Get the button that opens the modal
-// const btn = document.getElementById("myButto");
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// this is for the first card
-
-const Art= document.getElementsByClassName("fullArticle");
-
-
-
-// // When the user clicks the button, open the form 
-// btn.onclick = function() {
-//   form.style.display = "block";
-// }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  popUp.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == popUp) {
-    popUp.style.display = "none";}
-  }
-
-  const btn = document.querySelector(".myButto");
- btn.addEventListener('click', ()=>{
-     window.open('http://127.0.0.1:5500/Form.html')
- });
+const btn = document.querySelector(".myButto");
+btn.addEventListener('click',()=>{
+    window.open('http://127.0.0.1:5500/Form.html')
+});
 
 // function for displayin full article
 const card = document.querySelector(".A");
 card.addEventListener('click', ()=>{
-    window.open('http://127.0.0.1:5500/artcle.html')
+   window.open('http://127.0.0.1:5500/articleOne.html')
 });
 
-const backY = document.querySelector(".back");
-backY.addEventListener('click', ()=>{
-    window.open('http://127.0.0.1:5500/opinionPage.html')
+const cardB = document.querySelector(".B");
+cardB.addEventListener('click', ()=>{
+   window.open('http://127.0.0.1:5500/articleTwo.html')
 });
 
+const cardC = document.querySelector(".C");
+cardC.addEventListener('click', ()=>{
+   window.open('http://127.0.0.1:5500/articleThree.html')
+});
 
+const cardD = document.querySelector(".D");
+cardD.addEventListener('click', ()=>{
+   window.open('http://127.0.0.1:5500/articleFour.html')
+});
+const cardE = document.querySelector(".E");
+cardE.addEventListener('click', ()=>{
+   window.open('http://127.0.0.1:5500/articleFive.html')
+})
 
+const butto = document.getElementsByClassName("SubmitF");
+butto.addEventListener('click', ()=>{
+    window.open('http://127.0.0.1:5500/opinions.html')
+});

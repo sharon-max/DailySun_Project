1af283dfc3ed9b8f68e26ece10a333e7d5db80ad
iamsharonjp <sharon@adadev.org.za>
2020-02-19 09:21:17 +0200

    added API

--- js/homePage.js
+++ js/homePage.js
@@ -20,3 +20,43 @@ setTimeout(function(){
 }, 5000);
 });
 
+
+
+const articleContent = document.querySelector('#newsFeed');
+
+const newsImage = document.querySelector('.image');
+
+// const key = 'bd5968b940f3419ca949b01b99b6e75a';
+
+
+    const base = 'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bd5968b940f3419ca949b01b99b6e75a';
+
+    // const query = `?apikey=${key}`;
+    const getNews = async () =>{
+
+    const response = await fetch (base);
+    const data = await response.json()
+    .then(data =>{
+    // return data;
+    // }
+    // getNews()
+    //     .then(data => console.log(data))
+    //     .catch(err => console.log(err));
+
+    newsData = data;
+    articleContent.innerHTML = `
+    <div id="newsFeed" class="ssNews">
+      <h2>${newsData.articles[9].description} </h2>
+      
+      <p>${newsData.articles[9].publishedAt}</p>
+    </div>`;
+      
+        newsImage.setAttribute('src', newsData.articles[9].urlToImage);
+        
+         console.log(newsData);
+    }).catch(err => console.log(err));
+
+   
+};
+
+getNews();

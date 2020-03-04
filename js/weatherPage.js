e9057d2274ef65600ae329fb8dc512e1b08b0857
livhuwani <livhuwani@adadev.org.za>
2020-03-04 10:37:52 +0200

    changed of forms

--- js/weatherPage.js
+++ js/weatherPage.js
@@ -230,3 +230,61 @@
 
 getWeathers();
 
+
+
+
+
+
+
+
+
+
+
+
+
+//local storage and filter
+
+//log in and out of the website
+  
+let count = 3;
+function validate() {
+  let un = document.myform.username.value;
+  let pw = document.myform.pword.value;
+  let valid = false;
+
+  const unArray = ["username1", "username2", "username3", "username4"];  // as many as you like - no comma after final entry
+  const pwArray = ["password1", "password2", "password3", "password4"];  // the corresponding passwords;
+  for (let i=0; i <unArray.length; i++) {
+  if ((un == unArray[i]) && (pw == pwArray[i])) {
+  valid = true;
+  break;
+  }
+  }
+
+  if (valid) {
+    alert ("Login was sucessfully processed.  You will be redirected to the members page now.");
+    window.location = "http://www.google.com";
+    return false;
+    }
+    const t = " tries";
+    if (count == 1) {t = " try"}
+    if (count >= 1) {
+    alert ("Invalid username and/or password.  You have " + count + t + " left.");
+    document.myform.username.value = "";
+    document.myform.pword.value = "";
+    setTimeout("document.myform.username.focus()", 25);
+    setTimeout("document.myform.username.select()", 25);
+    count --;
+    }
+    else {
+      alert ("Still incorrect! You have no more tries left!");
+      document.myform.username.value = "No more tries allowed!";
+      document.myform.pword.value = "";
+      document.myform.username.disabled = true;
+      document.myform.pword.disabled = true;
+      return false;
+      }
+    }      
+
+
+

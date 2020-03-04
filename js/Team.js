6bae5e358d5bc99cd3359bef4c367bd8824c095a
iamsharonjp <sharon@adadev.org.za>
2020-03-04 10:38:28 +0200

    added changes on team js

--- js/Team.js
+++ js/Team.js
@@ -5,3 +5,37 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.left = "-100vw";
   }
+
+// popup and forms 
+  
+// Get the <span> element that closes the modal
+const close = document.querySelector(".pop-content");
+const zip = document.querySelector(".zip");
+
+// When the user clicks on <span> (x), close the popup
+zip.onclick = function() {
+  close.style.display = "none";
+}
+
+
+const popup = document.querySelector('.pop-content');
+$(document).ready( function() {
+
+// popup auto functionh
+setTimeout(function(){ 
+
+    //makes popup display
+    popup.classList.remove('d-none'); 
+}, 5000);
+});
+
+
+const button = document.querySelector('.login');
+ button.addEventListener('click', ()=>{
+     window.open('http://127.0.0.1:5500/logIn.html')
+ });
+
+ const butto = document.querySelector('.signup');
+ butto.addEventListener('click', ()=>{
+     window.open('http://127.0.0.1:5500/SignUp.html')
+ });
